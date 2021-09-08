4ac74a0c8fdc81aa45436a3508813d03c208af98
anyongc <ayc_mail@163.com>
2021-09-08 14:40:27 +0800

    Update ContentView.swift
    zailaiyici

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! nihao ")
+        Text("Hello, world! nihao zailaiyici ")
             .padding()
     }
 }
