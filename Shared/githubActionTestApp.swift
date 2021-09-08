8d90ce8f1c1fec79e0521711df7ce19eb387ca78
qxv2184 <ayc_mail@163.com>
2021-09-08 11:14:28 +0800

    project init

---
+++ Shared/githubActionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubActionTestApp.swift
+//  Shared
+//
+//  Created by qxv2184 on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct githubActionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
